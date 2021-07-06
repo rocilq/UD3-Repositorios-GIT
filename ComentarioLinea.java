b93d72ee562f97e8480fa63ca86714a2ee35cf03
rocilq <roziolq@gmail.com>
2021-07-06 13:06:39 +0200

    java

---
+++ ComentarioLinea.java
@@ -0,0 +1,9 @@
+
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+	}
+
+}
---
+++ ComentarioVariasLineas.java
@@ -0,0 +1,16 @@
+
+public class ComentarioVariasLineas {
+
+	public static void main(String[] args) {
+		/*System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");*/
+
+		for (int i=0; i<6 ; i++) {
+			System.out.print(i+" ");
+		}
+
+		System.out.println("FIN DE PROGRAMA");
+
+	}
+
+}
